36bad6165014fb2a30bf4bce81ea911a937e1147
Leonardo <leonardo.neves@bennu.tv>
2020-11-26 18:09:50 -0300

    :sparkles: Implements control lights

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
 const tmi = require('tmi.js');
-
 require('dotenv').config()
 
-// Define configuration options
+const lights = require('./service/lights');
+
 const opts = {
-  identity: {
-    username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN
-  },
-  channels: [
-    process.env.CHANNEL_NAME
-  ]
+	identity: {
+		username: process.env.BOT_USERNAME,
+		password: process.env.OAUTH_TOKEN
+	},
+	channels: [
+		process.env.CHANNEL_NAME
+	]
 };
-// Create a client with our options
+
 const client = new tmi.client(opts);
 
-// Register our event handlers (defined below)
+
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 
-// Connect to Twitch:
+
 client.connect();
 
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
 
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
+async function onMessageHandler(target, context, msg, self) {
+	if (self) { return; } 
 
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }
+
+	const commandName = msg.trim();
+
+	switch (commandName) {
+		case "!light-on":
+			await lights.on();
+			break;
+		case "!light-off":
+			await lights.off();
+			break;
+		case "!light-color":
+			await lights.color();
+			break;
+	
+		default:
+			break;
+	}
 }
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
+
+function onConnectedHandler(addr, port) {
+	console.log(`* Connected to ${addr}:${port}`);
 }
